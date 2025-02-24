850e264f748b20e452f7ab50862497ad0422e2b4
chesterh <cheaterh@stedent.s21-school.ru>
2025-02-24 12:55:35 +0300

    add keyboard

--- app/bot/keyboards/kbs.py
+++ app/bot/keyboards/kbs.py
@@ -11,6 +11,27 @@ def main_keyboard(user_id: int, first_name: str)->ReplyKeyboardMarkup:
     kb.button(text="у кого книга" )#TODO добавить поиск у кого книга
     if user_id == settings.ADNIN_ID:
         kb.button(text="Админ панель")
-    kb.adjust(1)
+    kb.adjust(2)
     return kb.as_markup(resize_keyboard=True)
     
+def back_keyboard() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    kb.button(text="🔙 Назад")
+    kb.adjust(1)
+    return kb.as_markup(resize_keyboard=True)
+
+def admin_keyboard(user_id: int) -> InlineKeyboardMarkup:
+    url_applications = f"{settings.BASE_SITE}/admin?admin_id={user_id}"
+    kb = InlineKeyboardBuilder()
+    kb.button(text="🏠 На главную", callback_data="back_home")
+    kb.button(text="📝 смотреть сколько книг взято", web_app=WebAppInfo(url=url_applications))
+    kb.adjust(1)
+    return kb.as_markup()
+
+
+def app_keyboard(user_id: int, first_name: str) -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    url_add_application = f'{settings.BASE_SITE}/form?user_id={user_id}&first_name={first_name}'
+    kb.button(text="📝 взять книгу", web_app=WebAppInfo(url=url_add_application))
+    kb.adjust(1)
+    return kb.as_markup()
